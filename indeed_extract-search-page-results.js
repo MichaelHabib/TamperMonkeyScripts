515733a1ba149217eb05a8fe6d67823f3363d1c7
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2023-08-03 11:15:22 +1000

    Create indeed_extract-search-page-results.js

---
+++ indeed_extract-search-page-results.js
@@ -0,0 +1,91 @@
+
+//window.open("https://www.seek.com.au/Allan-Hall-HR-seadan-jobs","_self")
+
+
+function extract_content(){
+
+// Get all the job card articles
+const jobCards = document.querySelectorAll('article[data-card-type="JobCard"]');
+
+// Create a new div to hold all the job card HTML
+let jobCardsContainer = document.querySelector('#jobCardsContainer');
+if (!jobCardsContainer) {
+  jobCardsContainer = document.createElement('div');
+  jobCardsContainer.id = 'jobCardsContainer';
+  document.body.appendChild(jobCardsContainer);
+} else {
+  jobCardsContainer.innerHTML = '';
+}
+
+// Loop through each job card article and extract the necessary data
+jobCards.forEach((article) => {
+  const titleElement = article.querySelector('[data-automation="jobTitle"]');
+  const title = titleElement ? titleElement.textContent.trim() : '';
+  console.log('Title:', title);
+
+  let titleHref = titleElement ? titleElement.getAttribute('href') : '';
+  console.log('titleHref:', titleHref);
+  if (titleHref) {
+    titleHref = 'https://www.seek.com.au' + titleHref;
+  }
+
+  const companyElement = article.querySelector('[data-automation="jobCompany"]');
+  const company = companyElement ? companyElement.textContent.trim() : '';
+  console.log('Company:', company);
+
+  const locationElement = article.querySelector('[data-automation="jobLocation"]');
+  const location = locationElement ? locationElement.textContent.trim() : '';
+  console.log('Location:', location);
+
+  const salaryElement = article.querySelector('[data-automation="jobSalary"]');
+  const salary = salaryElement ? salaryElement.textContent.trim() : '';
+  console.log('Salary:', salary);
+
+  const classificationElement = article.querySelector('[data-automation="jobClassification"]');
+  const classification = classificationElement ? classificationElement.textContent.trim() : '';
+  console.log('Classification:', classification);
+
+  const subClassificationElement = article.querySelector('[data-automation="jobSubClassification"]');
+  const subClassification = subClassificationElement ? subClassificationElement.textContent.trim() : '';
+  console.log('Sub Classification:', subClassification);
+
+  const descriptionElement = article.querySelector('[data-automation="jobShortDescription"]');
+  const description = descriptionElement ? descriptionElement.textContent.trim() : '';
+  console.log('Description:', description);
+
+  // Extract the list items from the description
+  let descriptionUl = '';
+  const descriptionUlElement = article.querySelector('ul');
+  if (descriptionUlElement) {
+    const listItems = descriptionUlElement.querySelectorAll('li');
+    for (let i = 0; i < listItems.length; i++) {
+      descriptionUl += `<li>${listItems[i].textContent.trim()}</li>`;
+    }
+  }
+  console.log('DescriptionUl: Generated:');
+  //console.log('DescriptionUl:', descriptionUl);
+
+  // Create the HTML code for this job card
+  const html = `
+    <div class="job-card">
+      ${titleHref ? `<h3><a href="${titleHref}" target="_blank">${title}</a></h3>` : `<h3>${title}</h3>`}
+      <p class="company">${company}</p>
+      <p class="location">${location}</p>
+      <p class="salary">${salary}</p>
+      <p class="classification">${classification}</p>
+      <p class="sub-classification">${subClassification}</p>
+      <ul class="descriptionUl">${descriptionUl}</ul>
+      <p class="description">${description}</p>
+      <p class="apply"><button class="apply-button"${titleHref ? `><a href="${titleHref}" target="_blank">Apply Now</a></button>` : ' disabled>Not available</button>'}</p>
+    </div>
+  `;
+
+  // Add the HTML code for this job card to the container
+  jobCardsContainer.innerHTML += html;
+});
+copy(document.querySelector('#jobCardsContainer'));
+console.log('DescriptionUl: Copied to Clipboard:');
+}
+
+// Run function
+extract_content()
